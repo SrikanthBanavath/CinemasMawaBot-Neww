3064c529ffc0c0bd9bdf96d3e70b988eb5389414
Srikanth Nayak <130298853+Srikanth5657@users.noreply.github.com>
2023-08-26 21:58:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -161,14 +161,17 @@ async def doc(bot, update):
 
 
 # Born to make history @LazyDeveloper !
-@Client.on_callback_query(filters.regex("pmnext"))
-async def pm_next_page(bot, query):
+@Client.on_callback_query(filters.regex(r"^next"))
+async def next_page(bot, query):
+
     ident, req, key, offset = query.data.split("_")
+    if int(req) not in [query.from_user.id, 0]:
+        return await query.answer("This Message is not for you dear. Don't worry you can send new one !", show_alert=True)
     try:
         offset = int(offset)
     except:
         offset = 0
-    search = PM_BUTTONS.get(key)
+    search = BUTTONS.get(key)
     if not search:
         await query.answer("You are using one of my old messages, please send the request again.", show_alert=True)
         return
@@ -230,13 +233,13 @@ async def pm_next_page(bot, query):
         off_set = offset - 10
     if n_offset == 0:
         btn.append(
-            [InlineKeyboardButton("◀️ 𝖡𝖠𝖢𝖪", callback_data=f"pmnext_{req}_{key}_{off_set}"),
+            [InlineKeyboardButton("◀️ 𝖡𝖠𝖢𝖪", callback_data=f"next_{req}_{key}_{off_set}"),
              InlineKeyboardButton(f"📃 Pages {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}", callback_data="pages")]                                  
         )
     elif off_set is None:
         btn.append(
             [InlineKeyboardButton(f"🗓 {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}", callback_data="pages"),
-             InlineKeyboardButton("NEXT ▶️", callback_data=f"pmnext_{req}_{key}_{n_offset}")])
+             InlineKeyboardButton("NEXT ▶️", callback_data=f"next_{req}_{key}_{n_offset}")])
     else:
         btn.append(
             [
@@ -1234,15 +1237,15 @@ async def auto_filter(client, msg, spoll=False):
     
     if offset != "":
         key = f"{message.chat.id}-{message.id}"
-        PM_BUTTONS[key] = search
+        BUTTONS[key] = search
         req = message.from_user.id if message.from_user else 0
         btn.append(
             [InlineKeyboardButton(text=f"📄 𝖯𝖠𝖦𝖤 1/{math.ceil(int(total_results) / 6)}", callback_data="pages"),
-            InlineKeyboardButton(text="𝖭𝖤𝖷𝖳 ▶️", callback_data=f"pmnext_{req}_{key}_{offset}")]
+            InlineKeyboardButton(text="𝖭𝖤𝖷𝖳 ▶️", callback_data=f"next_{req}_{key}_{offset}")]
         )
     else:
         btn.append(
-            [InlineKeyboardButton(text="📄 𝖯𝖠𝖦𝖤 1/1", callback_data="pages")]
+            [InlineKeyboardButton(text="🗓 1/1", callback_data="pages")]
         )
 
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
