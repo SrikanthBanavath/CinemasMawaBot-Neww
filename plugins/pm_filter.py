b50a624cd437a14746b9a39c6819d8c50ddfbe05
Srikanth Nayak <130298853+Srikanth5657@users.noreply.github.com>
2023-08-29 23:49:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -698,7 +698,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.START_TXT.format(query.from_user.mention, temp.B_NAME),
+            text=script.START_TXT.format(query.from_user.mention, temp.B_LINK),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
