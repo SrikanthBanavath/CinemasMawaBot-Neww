7f57f8698ef5af11a00535780c9c5178246a4b75
Srikanth Nayak <130298853+Srikanth5657@users.noreply.github.com>
2023-08-31 23:48:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1595,18 +1595,6 @@ async def advantage_spell_chok(client, msg):
         text=("Cant Find it Bro.. Check Your spelling")),
         reply_markup=InlineKeyboardMarkup(btn),
         reply_to_message_id=msg.id
-    )
-    try:
-        if settings['auto_delete']:
-            await asyncio.sleep(120)
-            await spell_check_del.delete()
-    except KeyError:
-            grpid = await active_connection(str(message.from_user.id))
-            await save_group_settings(grpid, 'auto_delete', True)
-            settings = await get_settings(message.chat.id)
-            if settings['auto_delete']:
-                await asyncio.sleep(120)
-                await spell_check_del.delete()
 
 
 async def manual_filters(client, message, text=False):
