8860d88b6082c3f0ad2eb7f0b6b302d17f3f724c
Srikanth Nayak <130298853+Srikanth5657@users.noreply.github.com>
2023-08-31 11:32:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
-BOT_USERNAME = environ.get('BOT_USERNAME' ,'CinemasMawa' )
+BOT_USERNAME = environ.get('BOT_USERNAME' ,'CinemasMawaBot' )
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
