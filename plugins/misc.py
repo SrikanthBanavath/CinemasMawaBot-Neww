19272ceb502ece0715ca43dfea9d1a12b05ace42
Srikanth Nayak <130298853+Srikanth5657@users.noreply.github.com>
2023-09-02 12:50:20 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -146,7 +146,7 @@ async def imdb_search(client, message):
         ]
         await k.edit('Here is what i found on IMDb', reply_markup=InlineKeyboardMarkup(btn))
     else:
-        await message.reply('Give me a movie / series Name')
+        await message.reply('Give me a movie / series Name\n\nEx: /imdb Kantara')
 
 @Client.on_callback_query(filters.regex('^imdb'))
 async def imdb_callback(bot: Client, quer_y: CallbackQuery):
