d8c11d4df2a763e7704a57acd88e8b28faf1af72
Srikanth Nayak <130298853+Srikanth5657@users.noreply.github.com>
2023-09-07 11:21:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -6,7 +6,7 @@ from Script import script
 from pyrogram import Client, filters, enums
 from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
+from database.ia_filterdb import Media, get_file_details, unpack_new_file_id, get_bad_files
 from database.users_chats_db import db
 from info import *
 from utils import get_settings, get_size, is_subscribed, save_group_settings, temp
