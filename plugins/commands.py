460db0fec4518485510e7f9f3761d8801470145c
Srikanth Nayak <130298853+Srikanth5657@users.noreply.github.com>
2024-03-10 12:48:01 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -28,7 +28,7 @@ async def start(client, message):
                     InlineKeyboardButton('♻️ Hᴇʟᴩ ♻️', url=f"https://t.me/{BOT_USERNAME}?start=help")
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
+        await message.reply(script.START_TXT_GROUP.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
         await asyncio.sleep(2) # 😢 https://github.com/LazyDeveloperr/LazyPrincess/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
