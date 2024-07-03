14c7604f90580c651381e322a5eaa3f05485cbc6
Srikanth Nayak <130298853+SrikanthBanavath@users.noreply.github.com>
2024-07-03 22:43:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -130,7 +130,7 @@ else:
 REPO_OWNER = "LazyDeveloperr"
 
 # Auto Delete For Group Message (Self Delete) #
-SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 1800))
+SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 86000))
 SELF_DELETE = environ.get('SELF_DELETE', True)
 if SELF_DELETE == "True":
     SELF_DELETE = True
